4854c6387d66bffda07bb0e645bc6b33a8561339
Min REN <ren78min@gmail.com>
2018-10-04 21:44:30 -0400

    Initialize

---
+++ src/main/java/me/mren/filepoller/FileEventObserver.java
@@ -0,0 +1,17 @@
+package me.mren.filepoller;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileEventObserver implements Observer {
+
+	@Override
+	public void update(final Observable o, final Object arg) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/me/mren/fileloader/FileEventObserverTest.java
@@ -0,0 +1,16 @@
+package me.mren.fileloader;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileEventObserverTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
